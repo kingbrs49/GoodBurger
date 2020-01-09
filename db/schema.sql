6dc8ce254ae1da25b6226da8238526682c4437b9
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-01-08 18:08:41 -0800

    connection.js filled out

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
-devoured BOOLEAN
+`id` int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+`burger_name` varchar(255),
+`devoured` BOOLEAN
 )
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Cheeseburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Bacon Burger', FALSE);
