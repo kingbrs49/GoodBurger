b8085b4b2e9e35aecf0c76d05793de87b631aa8e
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-01-10 21:18:13 -0800

    I have no idea if this is done or working

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,37 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function () {
+    selectAll: function (tableName, cb) {
         var queryString = "SELECT * FROM ?";
-        connection.query(queryString, [], function (err, result) {
-            if (err) throw err;
-            console.log(result);
+        connection.query(queryString, [tableName], function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                cb(result);
+                console.log(result);
+            }
         });
     },
-    selectOne: function () {
-        var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [], function (err, result) {
-            if (err) throw err;
-            console.log(result);
+    insertOne: function (tableName, column, value, cb) {
+        var queryString = "INSERT * INTO ??";
+        connection.query(queryString, [tableName, column, value], function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                cb(result);
+                console.log(result);
+            }
         });
     },
-    updateOne: function () {
+    updateOne: function (tableName, colVal, boolean, colName, condition, cb) {
         var queryString = "UPDATE * FROM ??";
-        connection.query(queryString, [], function (err, result) {
-            if (err) throw err;
-            console.log(result);
+        connection.query(queryString, [tableName, colVal, boolean, colName, condition], function (err, result) {
+            if (err) {
+                throw err;
+            } else {
+                cb(result);
+                console.log(result);
+            }
         });
     }
 }
--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+var exphbs = require("express-handlebars");
 var express = require("express");
+var mysql = require("mysql");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
 
 var router = express.Router();
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
