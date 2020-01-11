ded36097326e1001a8001225d6d09be29f767217
Paulo Camacho <paulo.ccamacho@gmail.com>
2020-01-10 22:11:28 -0800

    I think I fixed the orm.js, or I irreparably screwed it up

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,31 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
 
 var orm = {
-    selectAll: function (tableName, cb) {
-        var queryString = "SELECT * FROM ?";
-        connection.query(queryString, [tableName], function (err, result) {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             } else {
@@ -12,9 +34,18 @@ var orm = {
             }
         });
     },
-    insertOne: function (tableName, column, value, cb) {
-        var queryString = "INSERT * INTO ??";
-        connection.query(queryString, [tableName, column, value], function (err, result) {
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = "INSERT * INTO" + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+        connection.query(queryString, vals, function (err, result) {
             if (err) {
                 throw err;
             } else {
@@ -23,9 +54,16 @@ var orm = {
             }
         });
     },
-    updateOne: function (tableName, colVal, boolean, colName, condition, cb) {
-        var queryString = "UPDATE * FROM ??";
-        connection.query(queryString, [tableName, colVal, boolean, colName, condition], function (err, result) {
+    updateOne: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             } else {
